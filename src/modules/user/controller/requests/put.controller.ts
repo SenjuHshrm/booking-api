156109b53649a7db3296f15d9d37ec3fac374897
ParadiseTeam69 <charl.becina@gmail.com>
2024-08-22 20:00:44 +0800

    account-verification

--- src/modules/user/controller/requests/put.controller.ts
+++ src/modules/user/controller/requests/put.controller.ts
@@ -1,32 +1,47 @@
-import { profileImgStorageMedia } from './../../../../middleware';
-import { Request, Response, Router } from 'express'
-import UserService from '../../user.service'
+import { profileImgStorageMedia } from "./../../../../middleware";
+import { Request, Response, Router } from "express";
+import UserService from "../../user.service";
 
 const putUserRoutes: Router = Router()
-  .put('/set-as-host/:propAppId', (req: Request, res: Response) => {
-    return UserService.setAsProprietor(res, req.body.userId, req.body.staycationId, req.params.propAppId)
+  .put("/set-as-host/:propAppId", (req: Request, res: Response) => {
+    return UserService.setAsProprietor(
+      res,
+      req.body.userId,
+      req.body.staycationId,
+      req.params.propAppId
+    );
   })
 
-  .put('/update-profile/:id', profileImgStorageMedia.single('img'), (req: Request, res: Response) => {
-    let { body } = req
-    let data = {
-      name: {
-        fName: body.fName,
-        mName: body.mName,
-        lName: body.lName,
-        xName: body.xName
-      },
-      contact: body.contact,
-      desc: {
-        description: body.desc,
-        hobbies: body.hobbies,
-        work: body.work,
-        favFood: body.favFood,
-        favPlace: body.favPlace
-      },
-      img: (req.file) ? `/profile-img/${body.email}/${req.file?.originalname}` : req.body.img
+  .put(
+    "/update-profile/:id",
+    profileImgStorageMedia.single("img"),
+    (req: Request, res: Response) => {
+      let { body } = req;
+      let data = {
+        name: {
+          fName: body.fName,
+          mName: body.mName,
+          lName: body.lName,
+          xName: body.xName,
+        },
+        contact: body.contact,
+        desc: {
+          description: body.desc,
+          hobbies: body.hobbies,
+          work: body.work,
+          favFood: body.favFood,
+          favPlace: body.favPlace,
+        },
+        img: req.file
+          ? `/profile-img/${body.email}/${req.file?.originalname}`
+          : req.body.img,
+      };
+      return UserService.updateUserProfile(res, data, req.params.id);
     }
-    return UserService.updateUserProfile(res, data, req.params.id)
-  })
+  )
 
-export default putUserRoutes
+  .put("/verification/update-profile/:id", (req: Request, res: Response) => {
+    return UserService.verificationUpdateProfile(res, req.body, req.params.id);
+  });
+
+export default putUserRoutes;
