c7b3d050d30ffe04a43b3a657578ebdf863a7d6a
EnzGyb <rdimasapit.bos@gmail.com>
2024-09-01 20:55:49 +0800

    fix message emitter

--- src/socket/events/message/chat.event.ts
+++ src/socket/events/message/chat.event.ts
@@ -7,12 +7,19 @@ import { Socket } from "socket.io";
 
 const chat = (socket: Socket) => {
   let receiveChat = async (data: IMessageInput) => {
-    let newMsg = new Message({ ...data });
-    let msg = newMsg
+    // let newMsg = new Message({ ...data });
+    // let msg = newMsg
+    //   .save()
+    //   .then((m: IMessageSchema) => m.populate("from"))
+    //   .then(m => m);
+    
+    // socket.to(data.roomId).emit("msg:chat:send", msg);
+    new Message({ ...data })
       .save()
-      .then((m: IMessageSchema) => m.populate("from"))
-      .then(m => m);
-    socket.to(data.roomId).emit("msg:chat:send", msg);
+      .then((m: IMessageSchema) => m.populate('from'))
+      .then(m => {
+        socket.to(data.roomId).emit('msg:chat:send', m)
+      })
   };
 
   socket.on("msg:chat:receive", receiveChat);
