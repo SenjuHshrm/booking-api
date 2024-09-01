94dd7f656c35e2069ece572641083a9a059b2184
ParadiseTeam69 <charl.becina@gmail.com>
2024-09-01 08:24:58 +0800

    chat socket

--- src/socket/events/message/chat.event.ts
+++ src/socket/events/message/chat.event.ts
@@ -1,16 +1,21 @@
-import { IMessageInput, IMessageSchema } from './../../../modules/message/message.interface';
-import Message from './../../../modules/message/schema/Message.schema'
-import { Socket } from 'socket.io'
+import {
+  IMessageInput,
+  IMessageSchema,
+} from "./../../../modules/message/message.interface";
+import Message from "./../../../modules/message/schema/Message.schema";
+import { Socket } from "socket.io";
 
 const chat = (socket: Socket) => {
   let receiveChat = async (data: IMessageInput) => {
-    let newMsg = new Message({ ...data })
-    let msg = newMsg.save().then((m: IMessageSchema) => m.populate('from')).then(m => m)
-    socket.to(data.roomId).emit('msg:chat:send', msg)
-  }
+    let newMsg = new Message({ ...data });
+    let msg = newMsg
+      .save()
+      .then((m: IMessageSchema) => m.populate("from"))
+      .then(m => m);
+    socket.to(data.roomId).emit("msg:chat:send", msg);
+  };
 
+  socket.on("msg:chat:receive", receiveChat);
+};
 
-  socket.on('msg:chat:receive', receiveChat)
-}
-
-export default chat
+export default chat;
--- src/socket/events/message/index.ts
+++ src/socket/events/message/index.ts
@@ -1,15 +1,16 @@
-import join from './join.event'
-import disconnect from './disconnect.event'
-import read from './read.event'
-import { Server, Socket } from 'socket.io'
+import join from "./join.event";
+import disconnect from "./disconnect.event";
+import read from "./read.event";
+import { Server, Socket } from "socket.io";
+import chat from "./chat.event";
 
 const MessageEvent = (io: Server) => {
+  io.of("/message").on("connection", (socket: Socket) => {
+    join(socket);
+    disconnect(socket);
+    read(socket);
+    chat(socket);
+  });
+};
 
-  io.of('/message').on('connection', (socket: Socket) => {
-    join(socket)
-    disconnect(socket)
-    read(socket)
-  })
-}
-
-export default MessageEvent
+export default MessageEvent;
